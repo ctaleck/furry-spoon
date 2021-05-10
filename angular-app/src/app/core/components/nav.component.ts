99091d21a2ebd22eda0364342f9e29383dbde696
Christopher Taleck <chris@taleck.com>
2021-05-10 16:35:19 -0700

    Add breakout link to nav

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -4,13 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-nav',
   template: `
     <nav class="menu">
-      <p class="menu-label">Menu</p>
+      <p class="menu-label">Legacy Menu</p>
       <ul class="menu-list">
         <a routerLink="/products" routerLinkActive="router-link-active">
-          <span>Products</span>
+          <span>Legacy Products</span>
+        </a>
+        <a href="https://breakout-tst.azureedge.net/">
+          <span>Breakout</span>
         </a>
         <a routerLink="/about" routerLinkActive="router-link-active">
-          <span>About</span>
+          <span>Lagacy About</span>
         </a>
       </ul>
     </nav>
